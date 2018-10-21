38fa357fdc6ba8b08bb85ca0c4bc87e8163465d7
Arif <44020819+azimarif@users.noreply.github.com>
2018-10-22 00:17:31 +0530

    Refactor logging test detail.

--- arrayLibraryTest.js
+++ arrayLibraryTest.js
@@ -1,5 +1,5 @@
 const assert = require('assert');
-const { assertEqual, assertDeepEqual } = require('./testLibrary.js');
+const { logTestCase } = require('./testLibrary.js');
 
 const {
   isEven, complimentary, filterOddNumbers, filterEvenNumbers,
@@ -7,10 +7,9 @@ const {
   reverseFibonacciSeries, maxNumberInAList, minNumberInAList, averageOfArray,
   mappingNamesLengthToArray, countOddNumbers, countEvenNumbers,
   countNumbersAboveValue, countNumbersBelowValue, findIndexOfNumber,
-  swapElements, sortArrayInAscending, loopThroughArray,
-  sortArrayInDescending, isAscending, isDescending, extractDigits,
-  isElementExists, uniqueArrayElements, combineTwoArrays, arrayIntersection,
-  uniqueElementsFromFirstArray, isArraySubSet, zipArrayElements, rotateArray,
+  isAscending, isDescending, extractDigits,
+  isElementExists, uniqueArrayElements, unionOfTwoArrays, arrayIntersection,
+  uniqueElementsFromFirstSet, isSubSet, zipArrayElements, rotateArray,
   partitionArray
 } = require('./arrayLibrary.js');
 
@@ -18,7 +17,8 @@ const {
 const testFilterOddNumbers = function(input, expectedOutput) {
   let actualOutput = filterOddNumbers(input);
   let message = 'filterOddNumbers';
-  assertDeepEqual({ input, expectedOutput, actualOutput, message });
+  console.log(logTestCase({ input, expectedOutput, actualOutput, message }));
+  assert.deepEqual(actualOutput, expectedOutput);
 }
 
 testFilterOddNumbers([0], []);
@@ -31,7 +31,8 @@ testFilterOddNumbers([1, 2, 3, 4, 5, 6, 7, 8], [1, 3, 5, 7]);
 const testFilterEvenNumbers = function(input, expectedOutput) {
   let actualOutput = filterEvenNumbers(input);
   let message = 'filterEvenNumbers';
-  assertDeepEqual({ input, expectedOutput, actualOutput, message });
+  console.log(logTestCase({ input, expectedOutput, actualOutput, message }));
+  assert.deepEqual(actualOutput, expectedOutput);
 }
 
 testFilterEvenNumbers([0], [0]);
@@ -44,7 +45,8 @@ testFilterEvenNumbers([1, 2, 3, 4, 5, 6, 7, 8], [2, 4, 6, 8]);
 const testCalculateSum = function(input, expectedOutput) {
   let actualOutput = calculateSum(input);
   let message = 'calculateSum';
-  assertDeepEqual({ input, expectedOutput, actualOutput, message });
+  console.log(logTestCase({ input, expectedOutput, actualOutput, message }));
+  assert.deepEqual(actualOutput, expectedOutput);
 }
 
 testCalculateSum([], 0);
@@ -57,7 +59,8 @@ testCalculateSum([30, 35, 20, 15], 100);
 const testReverseArray = function(input, expectedOutput) {
   let actualOutput = reverseArray(input);
   let message = 'reverseArray';
-  assertDeepEqual({ input, expectedOutput, actualOutput, message });
+  console.log(logTestCase({ input, expectedOutput, actualOutput, message }));
+  assert.deepEqual(actualOutput, expectedOutput);
 }
 
 testReverseArray([], []);
@@ -70,7 +73,8 @@ testReverseArray([30, 35, 20, 15], [15, 20, 35, 30]);
 const testselectEverySecondNumber = function(input, expectedOutput) {
   let actualOutput = selectEverySecondNumber(input);
   let message = 'selectEverySecondNumber';
-  assertDeepEqual({ input, expectedOutput, actualOutput, message });
+  console.log(logTestCase({ input, expectedOutput, actualOutput, message }));
+  assert.deepEqual(actualOutput, expectedOutput);
 }
 
 testselectEverySecondNumber([], []);
@@ -83,7 +87,8 @@ testselectEverySecondNumber([30, 35, 20, 15], [30, 20]);
 const testReverseFibonacciSeries = function(input, expectedOutput) {
   let actualOutput = reverseFibonacciSeries(input);
   let message = 'reverseFibonacciSeries';
-  assertDeepEqual({ input, expectedOutput, actualOutput, message });
+  console.log(logTestCase({ input, expectedOutput, actualOutput, message }));
+  assert.deepEqual(actualOutput, expectedOutput);
 }
 
 testReverseFibonacciSeries(0, [0]);
@@ -96,7 +101,8 @@ testReverseFibonacciSeries(7, [8, 5, 3, 2, 1, 1, 0]);
 const testMaxNumberInAList = function(input, expectedOutput) {
   let actualOutput = maxNumberInAList(input);
   let message = 'maxNumberInAList';
-  assertDeepEqual({ input, expectedOutput, actualOutput, message });
+  console.log(logTestCase({ input, expectedOutput, actualOutput, message }));
+  assert.deepEqual(actualOutput, expectedOutput);
 }
 
 testMaxNumberInAList([-3, -5], -3);
@@ -109,7 +115,8 @@ testMaxNumberInAList([30, 35, 20, 15], 35);
 const testMinNumberInAList = function(input, expectedOutput) {
   let actualOutput = minNumberInAList(input);
   let message = 'minNumberInAList';
-  assertDeepEqual({ input, expectedOutput, actualOutput, message });
+  console.log(logTestCase({ input, expectedOutput, actualOutput, message }));
+  assert.deepEqual(actualOutput, expectedOutput);
 }
 
 testMinNumberInAList([-3, -5], -5);
@@ -122,7 +129,8 @@ testMinNumberInAList([30, 35, 20, 15], 15);
 const testAverageOfArray = function(input, expectedOutput) {
   let actualOutput = averageOfArray(input);
   let message = 'averageOfArray';
-  assertDeepEqual({ input, expectedOutput, actualOutput, message });
+  console.log(logTestCase({ input, expectedOutput, actualOutput, message }));
+  assert.deepEqual(actualOutput, expectedOutput);
 }
 
 testAverageOfArray([-3, -5], -4.00);
@@ -135,7 +143,8 @@ testAverageOfArray([30, 35, 20, 15], 25.00);
 const testMappingNamesLengthToArray = function(input, expectedOutput) {
   let actualOutput = mappingNamesLengthToArray(input);
   let message = 'mappingNamesLengthToArray';
-  assertDeepEqual({ input, expectedOutput, actualOutput, message });
+  console.log(logTestCase({ input, expectedOutput, actualOutput, message }));
+  assert.deepEqual(actualOutput, expectedOutput);
 }
 
 testMappingNamesLengthToArray(['Arif', 'Azim', 'Ansari'], [4, 4, 6]);
@@ -148,7 +157,8 @@ testMappingNamesLengthToArray(['0', '0'], [1, 1]);
 const testCountOddNumbers = function(input, expectedOutput) {
   let actualOutput = countOddNumbers(input);
   let message = 'countOddNumbers';
-  assertDeepEqual({ input, expectedOutput, actualOutput, message });
+  console.log(logTestCase({ input, expectedOutput, actualOutput, message }));
+  assert.deepEqual(actualOutput, expectedOutput);
 }
 
 testCountOddNumbers([], 0);
@@ -161,7 +171,8 @@ testCountOddNumbers([30, 35, 20, 15], 2);
 const testCountEvenNumbers = function(input, expectedOutput) {
   let actualOutput = countEvenNumbers(input);
   let message = 'countEvenNumbers';
-  assertDeepEqual({ input, expectedOutput, actualOutput, message });
+  console.log(logTestCase({ input, expectedOutput, actualOutput, message }));
+  assert.deepEqual(actualOutput, expectedOutput);
 }
 
 testCountEvenNumbers([], 0);
@@ -174,7 +185,8 @@ testCountEvenNumbers([30, 35, 20, 15], 2);
 const testCountNumbersAboveValue = function(input, value, expectedOutput) {
   let actualOutput = countNumbersAboveValue(input, value);
   let message = 'countNumbersAboveValue';
-  assertDeepEqual({ input, expectedOutput, actualOutput, message });
+  console.log(logTestCase({ input, expectedOutput, actualOutput, message }));
+  assert.deepEqual(actualOutput, expectedOutput);
 }
 
 testCountNumbersAboveValue([], 5, 0);
@@ -187,7 +199,8 @@ testCountNumbersAboveValue([30, 35, 20, 15], 22, 2);
 const testCountNumbersBelowValue = function(input, value, expectedOutput) {
   let actualOutput = countNumbersBelowValue(input, value);
   let message = 'countNumbersBelowValue';
-  assertDeepEqual({ input, expectedOutput, actualOutput, message });
+  console.log(logTestCase({ input, expectedOutput, actualOutput, message }));
+  assert.deepEqual(actualOutput, expectedOutput);
 }
 
 testCountNumbersBelowValue([], 5, 0);
@@ -200,7 +213,8 @@ testCountNumbersBelowValue([30, 35, 20, 15], 22, 2);
 const testFindIndexOfNumber = function(input, value, expectedOutput) {
   let actualOutput = findIndexOfNumber(input, value);
   let message = 'findIndexOfNumber';
-  assertDeepEqual({ input, expectedOutput, actualOutput, message });
+  console.log(logTestCase({ input, expectedOutput, actualOutput, message }));
+  assert.deepEqual(actualOutput, expectedOutput);
 }
 
 testFindIndexOfNumber([], 5, -1);
@@ -213,33 +227,36 @@ testFindIndexOfNumber([30, 35, 22, 20, 15], 22, 2);
 const testIsAscending = function(input, expectedOutput) {
   let actualOutput = isAscending(input);
   let message = 'isAscending';
-  assertDeepEqual({ input, expectedOutput, actualOutput, message });
+  console.log(logTestCase({ input, expectedOutput, actualOutput, message }));
+  assert.deepEqual(actualOutput, expectedOutput);
 }
 
 testIsAscending([], true);
 testIsAscending([0], true);
 testIsAscending([0, 1, 2], true);
-testIsAscending([-5, -2, 0, 1, -4], false);
-testIsAscending([30, 35, 22, 20, 15], false);
+testIsAscending([-5, -2, 0, 1, 4], true);
+testIsAscending([35, 30, 22, 20, 15], false);
 
 //Descending Order
 const testIsDescending = function(input, expectedOutput) {
   let actualOutput = isDescending(input);
   let message = 'isDescending';
-  assertDeepEqual({ input, expectedOutput, actualOutput, message });
+  console.log(logTestCase({ input, expectedOutput, actualOutput, message }));
+  assert.deepEqual(actualOutput, expectedOutput);
 }
 
 testIsDescending([], true);
 testIsDescending([0], true);
-testIsDescending([0, 1, 2], false);
-testIsDescending([-5, -2, 0, 1, -4], false);
-testIsDescending([30, 35, 22, 20, 15], false);
+testIsDescending([2,1,0], true);
+testIsDescending([-5, -2, 0, 1, 4], false);
+testIsDescending([35, 30, 22, 20, 15], true);
 
 //Extract Digit
 const testExtractDigits = function(input, expectedOutput) {
   let actualOutput = extractDigits(input);
   let message = 'extractDigits';
-  assertDeepEqual({ input, expectedOutput, actualOutput, message });
+  console.log(logTestCase({ input, expectedOutput, actualOutput, message }));
+  assert.deepEqual(actualOutput, expectedOutput);
 }
 
 testExtractDigits('', []);
@@ -252,33 +269,36 @@ testExtractDigits(4, [4]);
 const testUniqueArrayElements = function(input, expectedOutput) {
   let actualOutput = uniqueArrayElements(input);
   let message = 'uniqueArrayElements';
-  assertDeepEqual({ input, expectedOutput, actualOutput, message });
+  console.log(logTestCase({ input, expectedOutput, actualOutput, message }));
+  assert.deepEqual(actualOutput, expectedOutput);
 }
 
 testUniqueArrayElements([], []);
 testUniqueArrayElements([0], [0]);
 testUniqueArrayElements([0, 1], [0, 1]);
 testUniqueArrayElements([-5, -2, 0, -5, 1, -4], [-5, -2, 0, 1, -4]);
-testUniqueArrayElements([30, 35, 22, 20, 15, 30], [30, 35, 22, 20, 15], );
+testUniqueArrayElements([30, 35, 22, 20, 15, 30], [30, 35, 22, 20, 15] );
 
 //Union of Two Arrays
-const testCombineTwoArrays = function(input, value, expectedOutput) {
-  let actualOutput = combineTwoArrays(input, value);
-  let message = 'combineTwoArrays';
-  assertDeepEqual({ input, expectedOutput, actualOutput, message });
+const testUnionOfTwoArrays = function(input, value, expectedOutput) {
+  let actualOutput = unionOfTwoArrays(input, value);
+  let message = 'UnionOfTwoArrays';
+  console.log(logTestCase({ input, expectedOutput, actualOutput, message }));
+  assert.deepEqual(actualOutput, expectedOutput);
 }
 
-testCombineTwoArrays([], [], []);
-testCombineTwoArrays([0], [0], [0]);
-testCombineTwoArrays([0, 1], [0, 1], [0, 1]);
-testCombineTwoArrays([-5, -2, 0, ], [-5, 1, -4], [-5, -2, 0, 1, -4]);
-testCombineTwoArrays([30, 35, 22], [20, 15, 30], [30, 35, 22, 20, 15], );
+testUnionOfTwoArrays([], [], []);
+testUnionOfTwoArrays([0], [0], [0]);
+testUnionOfTwoArrays([0, 1], [0, 1], [0, 1]);
+testUnionOfTwoArrays([-5, -2, 0, ], [-5, 1, -4], [-5, -2, 0, 1, -4]);
+testUnionOfTwoArrays([30, 35, 22], [20, 15, 30], [30, 35, 22, 20, 15], );
 
 //Intersection of two Arrays
 const testArrayIntersection = function(input, value, expectedOutput) {
   let actualOutput = arrayIntersection(input, value);
   let message = 'arrayIntersection';
-  assertDeepEqual({ input, expectedOutput, actualOutput, message });
+  console.log(logTestCase({ input, expectedOutput, actualOutput, message }));
+  assert.deepEqual(actualOutput, expectedOutput);
 }
 
 testArrayIntersection([], [], []);
@@ -288,23 +308,25 @@ testArrayIntersection([-5, -2, 0, ], [-5, 1, -4], [-5]);
 testArrayIntersection([30, 35, 22], [20, 15, 30, 35], [30, 35]);
 
 //Unique Element from first Array
-const testUniqueElementsFromFirstArray = function(input, value, expectedOutput) {
-  let actualOutput = uniqueElementsFromFirstArray(input, value);
-  let message = 'uniqueElementsFromFirstArray';
-  assertDeepEqual({ input, expectedOutput, actualOutput, message });
+const testUniqueElementsFromFirstSet = function(input, value, expectedOutput) {
+  let actualOutput = uniqueElementsFromFirstSet(input, value);
+  let message = 'uniqueElementsFromFirstSet';
+  console.log(logTestCase({ input, expectedOutput, actualOutput, message }));
+  assert.deepEqual(actualOutput, expectedOutput);
 }
 
-testUniqueElementsFromFirstArray([], [], []);
-testUniqueElementsFromFirstArray([0], [0], []);
-testUniqueElementsFromFirstArray([0, 1], [0, 1], []);
-testUniqueElementsFromFirstArray([-5, -2, 0, ], [-5, 1, -4], [-2, 0]);
-testUniqueElementsFromFirstArray([30, 35, 22], [20, 15, 30, 35], [22]);
+testUniqueElementsFromFirstSet([], [], []);
+testUniqueElementsFromFirstSet([0], [0], []);
+testUniqueElementsFromFirstSet([0, 1], [0, 1], []);
+testUniqueElementsFromFirstSet([-5, -2, 0, ], [-5, 1, -4], [-2, 0]);
+testUniqueElementsFromFirstSet([30, 35, 22], [20, 15, 30, 35], [22]);
 
 //Check if second Array is subset of first Array
 const testIsArraySubSet = function(input, value, expectedOutput) {
-  let actualOutput = isArraySubSet(input, value);
-  let message = 'isArraySubSet';
-  assertDeepEqual({ input, expectedOutput, actualOutput, message });
+  let actualOutput = isSubSet(input, value);
+  let message = 'isSubSet';
+  console.log(logTestCase({ input, expectedOutput, actualOutput, message }));
+  assert.deepEqual(actualOutput, expectedOutput);
 }
 
 testIsArraySubSet([], [], true);
@@ -317,20 +339,22 @@ testIsArraySubSet([30, 35, 22], [20, 15, 30, 35], false);
 const testZipArrayElements = function(input, value, expectedOutput) {
   let actualOutput = zipArrayElements(input, value);
   let message = 'zipArrayElements';
-  assertDeepEqual({ input, expectedOutput, actualOutput, message });
+  console.log(logTestCase({ input, expectedOutput, actualOutput, message }));
+  assert.deepEqual(actualOutput, expectedOutput);
 }
 
 testZipArrayElements([], [], []);
 testZipArrayElements([0], [0], [[0, 0]]);
 testZipArrayElements([0, 1], [0, 1], [[0, 0], [1, 1]]);
 testZipArrayElements([-5, -2, 0, ], [-5, 1, -4], [[-5, -5], [-2, 1], [0, -4]]);
-testZipArrayElements([30, 35, 22, 35], [20, 15, 30], [[30, 20], [35, 15], [22, 30], [35, undefined]], );
+testZipArrayElements([30, 35, 22, 35], [20, 15, 30], [[30, 20], [35, 15], [22, 30], [35, undefined]] );
 
 //Rotate an Array by position
 const testRotateArray = function(input, value, expectedOutput) {
   let actualOutput = rotateArray(input, value);
   let message = 'rotateArray';
-  assertDeepEqual({ input, expectedOutput, actualOutput, message });
+  console.log(logTestCase({ input, expectedOutput, actualOutput, message }));
+  assert.deepEqual(actualOutput, expectedOutput);
 }
 
 testRotateArray([], 5, [undefined]);
@@ -343,7 +367,8 @@ testRotateArray([30, 35, 22, 20, 15], 22, [22, 20, 15, 30, 35]);
 const testPartitionArray = function(input, value, expectedOutput) {
   let actualOutput = partitionArray(input, value);
   let message = 'partitionArray';
-  assertDeepEqual({ input, expectedOutput, actualOutput, message });
+  console.log(logTestCase({ input, expectedOutput, actualOutput, message }));
+  assert.deepEqual(actualOutput, expectedOutput);
 }
 
 testPartitionArray([], 5, [[], []]);
